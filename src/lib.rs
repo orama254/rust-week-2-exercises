bfc5a4aae28a3a38cc4480ca8d4d2e1bfa8c8b38
orama254 <orama209@gmail.com>
2025-06-27 21:26:20 +0300

    chore: fix formating

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ pub fn swap_endian_u32(num: u32) -> [u8; 4] {
 
 pub fn parse_satoshis(input: &str) -> Result<u64, String> {
     // TODO: Parse input string to u64, return error string if invalid
-        input
+    input
         .parse()
         .map_err(|_| "Invalid satoshi amount".to_string())
 }
